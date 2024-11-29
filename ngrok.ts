4d4cb67032612aa58164917422e8ce7062d740aa
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2024-11-29 12:42:42 +0530

    added happy flow

--- ngrok.ts
+++ ngrok.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import routes from "./routes";
-import jsonPath from "jsonpath";
 
 const app = express();
 
@@ -12,19 +10,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
-app.use("/api", routes);
+app.post("/", (req: any, res: any) => {
+  res.send({ success: true });
+});
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 4040;
 
 app.listen(PORT, () => {
-  const data = {
-    context: {
-      location: {
-        citu: "adsa",
-      },
-    },
-  };
-
-  console.log("js", jsonPath.query(data, "$.context.location.citu"));
   console.log(`Server running at ${PORT}`);
 });
--- src/services/confirm.ts
+++ src/services/confirm.ts
@@ -1,5 +1,17 @@
+import extractPayloadData from "../utils/extract-payload-data";
+import { resolveTemplate } from "../utils/template_parser";
+import on_confirm_template from "../templates/on_confirm.json";
+import { sendResponse } from "../utils/api";
+
 const handleConfirmRequest = (payload: any) => {
   console.log("confirm payload: ", payload);
+
+  const extarctedData = extractPayloadData(payload, "confirm");
+  // store in cache
+  // valdiation
+  // if(l2.template) { return }
+  const responsePayload = resolveTemplate(on_confirm_template, extarctedData);
+  sendResponse(responsePayload, "confirm");
 };
 
 export default handleConfirmRequest;
