6a9493e703a723faeeff4e2d5306321c073cd48a
Shivang Dudani <shivangdudani113@gmail.com>
2024-12-12 17:21:26 +0530

    updating_env_file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const sendResponse = async (data: any, type: any, queryParam?: Record<string, string>) => {
   try {
-    // Base URL
-    const baseUrl = `https://8810-182-156-19-250.ngrok-free.app/mock/${type}`
+    // Retrieve the base URL from the environment variable
+    const baseUrl = `${process.env.API_SERVICE_LAYER}/mock/${type}`;
 
     // Append query parameters if provided
     const url = queryParam
       ? `${baseUrl}?${new URLSearchParams(queryParam).toString()}`
       : baseUrl;
-    
+
     // Send the POST request
     const response = await axios.post(url, data);
-    
+
     // Log the response
     console.log("response>", response.data);
   } catch (error) {
