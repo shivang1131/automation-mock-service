fede9b301f9a2949ad0ffe831fedf593993b038a
Shivang Dudani <shivangdudani113@gmail.com>
2024-12-16 11:51:30 +0530

    dynamic

--- src/services/first_search.ts
+++ src/services/first_search.ts
@@ -11,10 +11,10 @@ const initiateFirstSearch = async (payload: any) => {
         const subscriber_url = payload.subscriberUrl
         if(payload?.initiateSearch){
         const extracted_data:any = {}
-        extracted_data["transaction_id"] = "7743a9e2-4fb5-487c-92b7-13ba8018f177"
-        extracted_data["message_id"] = "8743e9e2-4fb5-487c-92b7-13ba8018f177"
-        extracted_data["timestamp"] = "2024-12-12T05:59:39.136Z"
-        extracted_data["city_code"] = "std:080"
+        extracted_data["transaction_id"] = generateRandomUUID()
+        extracted_data["message_id"] = generateRandomUUID()
+        extracted_data["timestamp"] = new Date().toISOString()
+        extracted_data["city_code"] = payload.city_code
         extracted_data["subscriber"] = subscriber_url
         const responsePayload = resolveTemplate(search_1, extracted_data,);
         sendResponse(responsePayload, "search",{ subscriberUrl: subscriber_url});
