169a1fabd1190012e39078fc5cbbb578bcaca956
Shivang Dudani <shivangdudani113@gmail.com>
2024-12-16 11:24:02 +0530

    hard coded transaction_ids

--- src/services/first_search.ts
+++ src/services/first_search.ts
@@ -14,7 +14,7 @@ const initiateFirstSearch = async (payload: any) => {
         extracted_data["transaction_id"] = "7743a9e2-4fb5-487c-92b7-13ba8018f177"
         extracted_data["message_id"] = "8743e9e2-4fb5-487c-92b7-13ba8018f177"
         extracted_data["timestamp"] = "2024-12-12T05:59:39.136Z"
-        extracted_data["city_code"] = "std:011"
+        extracted_data["city_code"] = "std:080"
         extracted_data["subscriber"] = subscriber_url
         const responsePayload = resolveTemplate(search_1, extracted_data,);
         sendResponse(responsePayload, "search",{ subscriberUrl: subscriber_url});
