d12113d356d6bd45e4957c87e6d615e0f0ed4ec4
shivang1131 <57249768+shivang1131@users.noreply.github.com>
2024-12-20 18:15:24 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import logger from "./logger";
 export const sendResponse = async (data: any, type: string, queryParam?: Record<string, string>) => {
   try {
     // Retrieve the base URL from the environment variable
-    const baseUrl = `${process.env.API_SERVICE_LAYER}/api/${type}`;
+    const baseUrl = `${process.env.API_SERVICE_LAYER}/mock/${type}`;
 
     // Append query parameters if provided
     const url = queryParam
