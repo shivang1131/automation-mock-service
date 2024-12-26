c332a1019fddfbd61a9fc1b60b4f9b709b35094b
Shivang Dudani <shivangdudani113@gmail.com>
2024-12-26 11:45:47 +0530

    adding status on_status

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import handleSerachRequest from "../services/search";
+import handleSearchRequest from "../services/search";
 import handleSelectRequest from "../services/select";
 import handleInitRequest from "../services/init";
 import handleConfirmRequest from "../services/confirm";
@@ -8,6 +8,8 @@ import handleOnSelectRequest from "../services/on_select"
 import handleOnInitRequest from "../services/on_init"
 import initiateFirstSearch from "../services/first_search"
 import handleOnConfirmRequest from "../services/on_confirm";
+import handleStatusRequest from "../services/status";
+import handleOnStatusRequest from "../services/on_status";
 import logger from "../utils/logger";
 
 export const handleRequest = async(req:Request, res: Response): Promise<any> => {
@@ -17,7 +19,7 @@ export const handleRequest = async(req:Request, res: Response): Promise<any> => 
     switch (action) {
       case "search":
         logger.info("search");
-        await handleSerachRequest(payload);
+        await handleSearchRequest(payload);
         return res.status(200).json({ message: {ack: { status: "ACK" }}});
       case "select":
         logger.info("select");
@@ -30,6 +32,10 @@ export const handleRequest = async(req:Request, res: Response): Promise<any> => 
       case "confirm":
         logger.info("confirm");
         await handleConfirmRequest(payload);
+        return res.status(200).json({ message: {ack: { status: "ACK" }}});
+      case "status":
+        logger.info("status");
+        await handleStatusRequest(payload);
         return res.status(200).json({ message: {ack: { status: "ACK" }}});
       case "on_search":
         logger.info("on_search");
@@ -47,6 +53,10 @@ export const handleRequest = async(req:Request, res: Response): Promise<any> => 
           logger.info("on_confirm");
           await handleOnConfirmRequest(payload);
           return res.status(200).json({ message: {ack: { status: "ACK" }}});
+        case "on_status":
+          logger.info("on_status");
+          await handleOnStatusRequest(payload);
+          return res.status(200).json({ message: {ack: { status: "ACK" }}});
       default:
         throw new Error(`Invalid request type ${action}`);
     }
@@ -58,66 +68,6 @@ export const handleRequest = async(req:Request, res: Response): Promise<any> => 
       .json({ error: true, message: e?.message || "Somthing went wrong" });
   }
 }
-export const handleBAPRequest = async (req: Request, res: Response): Promise<any> => {
-  try {
-    const action = req.params.action;
-    const payload = req.body;
-    switch (action) {
-      case "search":
-        logger.info("search");
-        await handleSerachRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      case "select":
-        logger.info("select");
-        await handleSelectRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      case "init":
-        logger.info("init");
-        await handleInitRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      case "confirm":
-        logger.info("confirm");
-        await handleConfirmRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      default:
-        throw new Error(`Invalid request type ${action}`);
-    }
-  } catch (e: any) {
-    logger.error("Error in handleBAPRequest", e);
-    res
-      .status(400)
-      .json({ error: true, message: e?.message || "Somthing went wrong" });
-  }
-};
-
-export const handleBPPrequest = async (req: Request, res: Response): Promise<any> => {
-  try {
-    const action = req.params.action;
-    const payload = req.body;
-    switch (action) {
-      case "on_search":
-        logger.info("on_search");
-        await handleOnSearchRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      case "on_select":
-        logger.info("on_select");
-        await handleOnSelectRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      case "on_init":
-        logger.info("on_init");
-        await handleOnInitRequest(payload);
-        return res.status(200).json({ message: {ack: { status: "ACK" }}});
-      default:
-        throw new Error(`Invalid request type ${action}`);
-    }
-  } catch (e: any) {
-    logger.error("Error in handleBPPrequest", e);
-    res
-      .status(400)
-      .json({ error: true, message: e?.message || "Somthing went wrong" });
-  }
-};
-
 
 export const triggerevent = async (req: Request, res: Response): Promise<any> => {
   try {
--- src/services/status.ts
+++ src/services/status.ts
@@ -1,21 +1,23 @@
 import extractPayloadData from "../utils/extract-payload-data";
 import { resolveTemplate } from "../utils/template_parser";
-import status from "../templates/status.json";
+import on_status from "../templates/on_status.json";
 import { sendResponse } from "../utils/api";
+import { RedisService } from "ondc-automation-cache-lib";
 import { generateRandomUUID } from "../utils/generate_uuids";
 import { getFromCache } from "../utils/redis";
 import { CACHE_DB_2 } from "../constants/constants";
 
-const handleOnConfirmRequest = async (payload: any) => {
+const handleStatusRequest = async (payload: any) => {
   let json_cache_data: any = await getFromCache(payload.context.transaction_id,CACHE_DB_2);
   const extracted_data:any = {}
-  extracted_data["message_id"] = generateRandomUUID()
-  extracted_data["timestamp"] = new Date().toISOString();
+  const time_stamp = new Date().toISOString();
+  extracted_data["timestamp"] = time_stamp
+  extracted_data["updated_at"] = time_stamp
   const combined_data = {...json_cache_data,...extracted_data}
-  const responsePayload = resolveTemplate(status, combined_data);
-  sendResponse(responsePayload, "status");
+  const responsePayload = resolveTemplate(on_status, combined_data);
+  sendResponse(responsePayload, "on_status");
   };
 
 
-  export default handleOnConfirmRequest;
+  export default handleStatusRequest;
 
